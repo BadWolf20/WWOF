12d8fd14c7954c79823f7858e945f7533ca9b78d
BadWolf20 <Roman.Pichugin20@gmail.com>
2022-01-16 12:30:07 +0700

    Preparation. Part 2
    Add load screen
    Add navigation

--- WWOF/AppDelegate.swift
+++ WWOF/AppDelegate.swift
@@ -11,11 +11,16 @@ import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+
+        let navigationController = UINavigationController.init(rootViewController: SignInViewController())
+
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 
